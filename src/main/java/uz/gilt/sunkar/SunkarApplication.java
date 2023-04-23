82b551464e9df05cc295fb63b8af91da013e7c80
sadirbaev <sadirbaevbaxit2@gmail.com>
2023-04-23 14:32:52 +0500

    initial commit

---
+++ src/main/java/uz/gilt/sunkar/SunkarApplication.java
@@ -0,0 +1,13 @@
+package uz.gilt.sunkar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunkarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunkarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/gilt/sunkar/SunkarApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.gilt.sunkar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunkarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
