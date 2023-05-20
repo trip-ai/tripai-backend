1940c0d87392282e2734dc0399507f189f75112e
sadirbaev <sadirbaevbaxit2@gmail.com>
2023-05-20 16:40:33 +0500

    Open api was configured for CORS

---
+++ src/main/java/uz/tripai/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package uz.tripai.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"));
+    }
+}
